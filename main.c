e8d133cbd66e62de4bedbd8299fe2dfcd24cc018
santxz45 <santos.cleldisson09@aluno.ifce.edu.br>
2024-11-19 07:30:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int linha, coluna, **matriz;
+    
+    scanf("%d %d", &linha, &coluna);
+    if(linha == 0 || coluna == 0){
+        printf("[vazio]");
+        return 0;
+    }
+    matriz = (int **)malloc(linha * sizeof(int));
+        if(matriz == NULL){
+        printf("Memória não alocada para as linhas.\n");
+        return 1;
+    }
+    for (int i = 0; i < linha; i++){
+    matriz[i] = (int *)malloc(coluna * sizeof(int));
+        if(matriz[i] == NULL){
+            printf("Memória não alocada para as colunas.\n");
+            return 1;
+        }
+    }
+        
+    for(int i = 0; i < linha; i++){
+        for (int j = 0; j < coluna; j++){
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    
+    for(int i = 0; i < linha; i++){
+        for(int j = 0; j < coluna; j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 
+        for (int i = 0; i < linha; i++){
+            free(matriz[i]);
+        }
+
+        free(matriz);
+    
     return 0;
 }
